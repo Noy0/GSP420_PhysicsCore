41e83d280eefba9b6e68d44cc32a209052e7d440
Noy0 <nalov0@gmail.com>
2018-03-11 01:43:20 -0700

    resort method order for PhysicsWorld
    easier to find methods now

--- MultiThread/Physics.h
+++ MultiThread/Physics.h
@@ -28,51 +28,49 @@ private:
 	int mNumOfObjects;
 
 public:
-	//Kinematic funtions
-	void displayCallback();	
-	bool IsKinematic(int id);
-	void SetShapePosition(int id, btVector3& position);
-	void SetShapeRotation(int id,btQuaternion& quat );
-	void SetShapeRotation(int id, btVector3& axis,float degree);
-	void RotateShapeOnX(int id, float degree);
-	void RotateShapeOnY(int id, float degree);
-	void RotateShapeOnZ(int id, float degree);
-	void wipePhysics();
-	void setPhysics();
-
-	//Create sphere
-	int CreateCollisionSphere(SphereInfo& info);
-	//Create box
-	int CreateCollisionBox(BoxInfo& info);
-	//create cylinder using box info
-	int PhysicsWorld::CreateCollisionCylinder(BoxInfo& info);
-
-	//dynamic functions
-
-	void SetAsKinematic(int id);
-	void ApplyCentralForce(int id,btVector3& force);
-
-	btVector3 GetShapePosition(int id);
-	btVector3 GetShapeRotation(int id);
-
-	void CreateWorld(WorldInfo& WldInfo);
-	void UpdateWorld(btScalar timeStep=(1/60), int maxSubSteps=1);
 	PhysicsWorld();
 	~PhysicsWorld();
+
+	void CreateWorld(WorldInfo& WldInfo);
+	void wipePhysics();
+
+	void UpdateWorld(btScalar timeStep = (1 / 60), int maxSubSteps = 1);
+
+	void displayCallback();
+
+	//int CreateCollision_Plane(PMatPlane& mat);
+	int CreateCollisionBox(BoxInfo& info);//int CreateCollisionBox(PMatBox& mat);
+	int CreateCollisionSphere(SphereInfo& info);//int CreateCollisionSphere(PMatSphere& mat);
+	//int CreateCollision_Capsule(BoxInfo& info);
+	int PhysicsWorld::CreateCollisionCylinder(BoxInfo& info);//int CreateCollisionCylinder(PMatCylinder& mat);
+
+	void PhysicsWorld::DeletePhysicsObject(int id);
+
+	bool IsKinematic(int id);
+	void SetAsKinematic(int id);
+
+	btVector3 GetShapePosition(int id);//D3DXVECTOR3 GetPosition(int id);
+	void SetShapePosition(int id, btVector3& position);//void SetPosition(int id, D3DXVECTOR3& position);
+
+	btVector3 GetLinearVelocity(int id);//D3DXVECTOR3 GetLinearVelocity(int id);
+	void SetLinearVelocity(int id, btVector3& vel);//void SetLinearVelocity(int id, D3DXVECTOR3& vel);
+
+	void ApplyCentralForce(int id, btVector3& force);//void ApplyCentralForce(int id, D3DXVECTOR3& force);
+	
+	btVector3 GetShapeRotation(int id);//D3DXQUATERNION GetRotation(int id);
+	void SetShapeRotation(int id,btQuaternion& quat );//void SetRotation(int id, D3DXQUATERNION& quat);
+	void SetShapeRotation(int id, btVector3& axis,float degree);//void SetRotation_Simple(int id, D3DXVECTOR3& axis, float degree);
+	void RotateShapeOnX(int id, float degree);//void RotateOnCoordAxis(int id, float angle, AxisID axis);
+	void RotateShapeOnY(int id, float degree);
+	void RotateShapeOnZ(int id, float degree);
 
 	btVector3 ConvertToBtVec(D3DXVECTOR3& old);
 	D3DXVECTOR3 convertToDxVec(btVector3& old);
 
+	//btQuaternion ConvertToBtQuat(D3DXQUATERNION& old);
 	D3DXQUATERNION ConvertToDxRot(btVector3& oldRot);
 
-
-	btVector3 GetLinearVelocity(int id);
-	void SetLinearVelocity(int id, btVector3& vel);
-
-	void PhysicsWorld::DeletePhysicsObject(int id);
-	
-
-
+	void setPhysics();
 };
 
 extern PhysicsWorld* gPhysics;
